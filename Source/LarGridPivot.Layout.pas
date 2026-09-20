7a52cb3c9cf358ad3c9eaea048437f68fd968a6a
larapipo <luis@larsoft.com.ar>
2026-09-20 17:39:59 -0300

    Migrate saved layouts for new VCL style theme enum

--- Source/LarGridPivot.Layout.pas
+++ Source/LarGridPivot.Layout.pas
@@ -9,7 +9,7 @@ uses
 type
   TLarPivotLayout = class
   public const
-    CurrentVersion = 2;
+    CurrentVersion = 3;
   public
     class function SaveToString(AFields: TLarPivotFields;
       AFilters: TLarPivotFilters; AShowRowTotals, AShowColumnTotals,
