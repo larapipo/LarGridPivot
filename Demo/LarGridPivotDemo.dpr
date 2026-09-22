6990b8666864d8e6a88813f7561351242a685fa2
larapipo <luis@larsoft.com.ar>
2026-09-22 13:10:00 -0300

    Make demo consume LarGridPivot through form units

--- Demo/LarGridPivotDemo.dpr
+++ Demo/LarGridPivotDemo.dpr
@@ -3,17 +3,9 @@ program LarGridPivotDemo;
 uses
   Vcl.Forms,
   Demo.Main in 'Demo.Main.pas' {FrmLarGridPivotDemo},
-  Demo.Gestion in 'Demo.Gestion.pas' {FrmLarGridPivotGestionDemo},
-  LarGridPivot.Types in '..\Source\LarGridPivot.Types.pas',
-  LarGridPivot.Fields in '..\Source\LarGridPivot.Fields.pas',
-  LarGridPivot.Filters in '..\Source\LarGridPivot.Filters.pas',
-  LarGridPivot.Model in '..\Source\LarGridPivot.Model.pas',
-  LarGridPivot.DataProvider in '..\Source\LarGridPivot.DataProvider.pas',
-  LarGridPivot.Engine in '..\Source\LarGridPivot.Engine.pas',
-  LarGridPivot.LayoutEngine in '..\Source\LarGridPivot.LayoutEngine.pas',
-  LarGridPivot.ViewInfo in '..\Source\LarGridPivot.ViewInfo.pas',
-  LarGridPivot.Layout in '..\Source\LarGridPivot.Layout.pas',
-  LarGridPivot.Grid in '..\Source\LarGridPivot.Grid.pas';
+  Demo.Gestion in 'Demo.Gestion.pas' {FrmLarGridPivotGestionDemo};
+
+{$R *.res}
 
 begin
   Application.Initialize;
